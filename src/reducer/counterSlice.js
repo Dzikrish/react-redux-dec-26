d5c6d79f26b5db1d2faba7685da8b129ebac04c0
dzikris <dzikri.subagja02@gmail.com>
2023-12-05 10:37:42 +0700

    reducer and store

---
+++ src/reducer/counterSlice.js
@@ -0,0 +1,26 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export const counterSlice = createSlice({
+  //Nama state
+  name: "counter",
+  //inisial state
+  initialState: {
+    value: 0,
+  },
+  //nama action reducer
+  reducers: {
+    increment: (state) => {
+      //actionnya ngapain
+      state.value += 1;
+    },
+    decrement: (state) => {
+      //actionnya ngapain
+      state.value -= 1;
+    },
+  },
+});
+
+//export actionnya ke reducer
+export const { increment, decrement } = counterSlice.actions;
+//export reducer biar bisa dipake di func lain
+export default counterSlice.reducer;
