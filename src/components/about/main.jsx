d70464380c584c54fddfbe1b3877bcbf90284def
dzikris <dzikri.subagja02@gmail.com>
2023-12-05 11:13:03 +0700

    Penambahan tampilan about

--- src/components/about/main.jsx
+++ src/components/about/main.jsx
@@ -1,12 +1,13 @@
-import React from 'react';
-import Button from '../common/button';
+import React from "react";
+import Button from "../common/button";
+import { useSelector } from "react-redux";
 
 const MainComponents = () => {
+  const counterRedux = useSelector((state) => state.counter.value);
   return (
     <div>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi, quam nemo
-      earum quia doloribus excepturi ad. Commodi quia culpa ex dignissimos
-      accusantium eos alias, sed voluptate voluptatem accusamus eum deleniti.
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi, quam nemo earum quia doloribus excepturi ad. Commodi quia culpa ex dignissimos accusantium eos alias, sed voluptate voluptatem accusamus eum deleniti.
+      <h1>Main Component {counterRedux}</h1>
       <Button>Haloo</Button>
     </div>
   );
--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -1,10 +1,16 @@
-import React from 'react';
-import MainComponents from '../components/about/main';
+import React from "react";
+import MainComponents from "../components/about/main";
+import { useDispatch } from "react-redux";
+import { decrement, increment } from "../reducer/counterSlice";
 
 const About = () => {
+  //inisialisasi dispatch kita
+  const dispatch = useDispatch();
   return (
     <>
       <MainComponents />
+      <button onClick={() => dispatch(increment())}>Increment</button>
+      <button onClick={() => dispatch(decrement())}>Decrement</button>
     </>
   );
 };
